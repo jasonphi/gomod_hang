6dca2d8b0bacac55f7a31634caf281a6ca654bc7
Jason Phillips <jason.phillips@vivint.com>
2022-12-15 17:08:45 -0500

    Added a dependency to b

--- b/badger.go
+++ b/badger.go
@@ -1,3 +1,9 @@
 package b
 
+import "github.com/jasonphi/gomod_hang/a/acorn"
+
 const Badger = "badger"
+
+func BestAcorn() acorn.Acorn {
+	return acorn.NorthernRedOak
+}
--- b/go.mod
+++ b/go.mod
@@ -1,3 +1,5 @@
 module github.com/jasonphi/gomod_hang/b
 
 go 1.16
+
+require github.com/jasonphi/gomod_hang/a v1.0.0
