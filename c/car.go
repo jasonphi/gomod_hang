7be2e77ee844660f7e8788d910642f8faba59a9e
Jason Phillips <jason.phillips@vivint.com>
2023-01-04 14:11:27 -0500

    Added c module

---
+++ c/car.go
@@ -0,0 +1,7 @@
+package c
+
+import "github.com/jasonphi/gomod_hang/b"
+
+const Car = "Mustang"
+
+const Badger = b.Badger
---
+++ c/go.mod
@@ -0,0 +1,5 @@
+module github.com/jasonphi/gomod_hang/c
+
+go 1.16
+
+require github.com/jasonphi/gomod_hang/b v1.0.0
